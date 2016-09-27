9424b7faaf67fe499331eda7797d9551f8fac007
Everton Silvestre <everton.srs@gmail.com>
2016-09-27 17:53:13 -0300

    Versão_2

---
+++ js/action.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+$('.slider').slider();
+$('.carousel.carousel-slider').carousel({full_width: true});
+$(".button-collapse").sideNav();
+$('.collapsible').collapsible();
+});
