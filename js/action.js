d69820729c3491ddac70518378310852f980805e
Everton Silvestre <everton.srs@gmail.com>
2016-09-29 01:57:26 -0300

    Versao_8

--- js/action.js
+++ js/action.js
@@ -1,6 +1,45 @@
 $(document).ready(function(){
 $('.slider').slider();
-$('.carousel.carousel-slider').carousel({full_width: true});
-$(".button-collapse").sideNav();
-$('.collapsible').collapsible();
+// $('.carousel.carousel-slider').carousel({full_width: true});
+ $('.button-collapse').sideNav();
+// $('.collapsible').collapsible();
+
 });
+
+// $('.sair').on('click', function (event) {
+
+//     // prevenir comportamento normal do link
+//     event.preventDefault();
+//     $('.button-collapse').sideNav('hide');
+//     // código a executar aqui
+// });
+
+$('.mobile').on('click', function (event) {
+
+    // prevenir comportamento normal do link
+    // event.preventDefault();
+    // $('.button-collapse').sideNav('hide');
+    // código a executar aqui
+    if ($(window).innerWidth() <= 992){
+     $(document).ready(function(){
+            $('.button-collapse').sideNav('hide');
+     });
+      
+   };
+});
+
+
+
+
+// $("a").click(function(evento){
+//     if ($(window).innerWidth() <= 976){
+//      $(document).ready(function(){
+//          $('.button-collapse').sideNav({closeOnClick: true});
+//      });
+     
+//    }else{
+//     $(document).ready(function(){
+//          $('.button-collapse').sideNav({closeOnClick: false});
+//      });  
+//    };
+// });
